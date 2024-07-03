93bcf2e80e9d9764be464eceaf84f9a3872f3174
Pradnya Sutar <158039930+pradnyasutar@users.noreply.github.com>
2024-07-03 11:43:25 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+import numpy as numpy
+import pickle
+import math
+from flask import Flask,resqest,jsonify,render_template
+
+app=Flask(__name__,template_folder="template",static_folder="staticfiles")## assign Flask=app
+model= pickle.load(open('build.pkl','rb'))## import model
+
+@app.route('/')
+def home():
+    return render_template('index.html') #read index.html
+
+@app.route('/predict',methods=['POST'])# transfer data from html to python/server
+
+def predict():
+    int_features=[int(x) for x in request.form.values()]# request for data values
+    final_features=[np.array(int_features)] # converting into array
+    prediction=model.predict(final_features)# predict
+    if prediction==0:
+         return render_template('index.html',prediction_text="Loan is Rejected").format(prediction)
+    else:
+        return render_template('index.html',prediction_text="Loan is Approved").format(prediction)
+           
+if __name__=="__main__":
+    app.run(host="0.0.0.0",port=8080)                         
